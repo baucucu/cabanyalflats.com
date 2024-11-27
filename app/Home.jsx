d91ae77fccd398fcf446b065462fb81d8cc9e741
Alex Raduca <alexandru.raduca@gmail.com>
2024-11-27 18:20:06 +0200

    updated footer

--- app/Home.jsx
+++ app/Home.jsx
@@ -182,16 +182,16 @@ export default function Home({ global, properties, totalUnits }) {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500">
-          © 2024 Cabanyal Flats. All rights reserved.
+          © {new Date().getFullYear()} Cabanyal Flats. All rights reserved.
         </p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Terms of Service
           </Link>
           <Link className="text-xs hover:underline underline-offset-4" href="#">
             Privacy
           </Link>
-        </nav>
+        </nav> */}
       </footer>
     </div>
   );
